2c2ef2fa86807ce2cf29d0b806641f6c4fab7b7a
fresh-joey <johserf30@gmail.com>
2025-02-25 13:58:34 +0100

    Log Archive Tool

---
+++ log-archive-tool/README.sh
@@ -0,0 +1,12 @@
+# README for Log Archive Tool
+
+## Description
+The Log Archive Tool is a shell script designed to archive log files from a specified directory. This tool helps in managing log files by compressing and moving them to an archive directory, thus saving disk space and keeping the log directory organized.
+
+## Features
+- Compresses log files using gzip
+- Moves compressed log files to an archive directory
+- Option to specify the log directory and archive directory
+- Option to specify the number of days to keep log files before archiving
+
+## Usage
