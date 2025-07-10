125c4f34f2b6da9645b3e7fbeea70e436be30039
fresh-joey <johserf30@gmail.com>
2025-07-10 09:09:43 +0100

    update deployment script

--- static-site-server/deploy.sh
+++ static-site-server/deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Variables
-LOCAL_DIR="/home/josephemmanuel/Documents/Devops/devops-projects/static-site-server"
+LOCAL_DIR="project/website"
 REMOTE_USER="ubuntu"
-REMOTE_HOST="54.209.176.20"
-REMOTE_DIR="/var/www/web"
-SSH_KEY="/home/josephemmanuel/Downloads/web-key.pem"
+REMOTE_HOST="server-ip"
+REMOTE_DIR="/var/www/website"
+SSH_KEY="/path/to/web-key.pem"
 
 # Rsync command
-echo "==========================="
+echo "---------------------------"
 echo "Syncing files..."
-echo "==========================="
+echo "---------------------------"
 
-rsync -avz -e "ssh -i $SSH_KEY" --delete $LOCAL_DIR/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
+rsync -avz --progress -e "ssh -i $SSH_KEY" --delete $LOCAL_DIR/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
 
 # Change file permission after sync
-echo "==========================="
+echo "---------------------------"
 echo "Updating file permissions..."
-echo "==========================="
+echo "---------------------------"
 ssh -i "$SSH_KEY" "$REMOTE_USER@$REMOTE_HOST" "sudo chown -R ubuntu:www-data $REMOTE_DIR && sudo chmod -R 755 $REMOTE_DIR" 
 
 # Print a message
